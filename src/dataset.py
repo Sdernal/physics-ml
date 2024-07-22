379b485b9c7aa8361501ad816a51b2e81664a3e7
Владислав Корзун <sdernal2@gmail.com>
2024-07-22 21:35:47 +0300

    Started with model

---
+++ src/dataset.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Union
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class PoissonDataset(Dataset):
+    def __init__(self, data_path: Union[Path, str], dt: float = 0.001, h: float = 1., rho: float = 1.):
+        data_archive = np.load(data_path)
+        assert len({'p', 'u', 'v'} & set(data_archive.keys())) == 3
+
+        self.pt = data_archive['p']
+        vt = data_archive['v']
+        ut = data_archive['u']
+        dx = dy = h
+        self.g = np.zeros_like(vt)
+        self.g[:, 1:-1, 1:-1] = rho / dt * ((ut[:,2:,1:-1] - ut[:,-2,1:-1]) / (2*dx)
+                                            + (vt[:,1:-1,2:] - vt[:,1:-1,:-2])/ (2*dy))
+
+    def __len__(self):
+        return self.pt.shape[0] - 2
+
+    def __getitem__(self, item):
+        return torch.Tensor(self.g[item+1]), torch.Tensor(self.pt[item+2])
+
+    @staticmethod
+    def collate_fn(batch):
+        g, p = list(zip(*batch))
+        return torch.stack(g, dim=0), torch.stack(p, dim=0)
+
---
+++ src/model.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+
+class Up(nn.Module):
+    def __init__(self, c1: int, c2: int, c_out: int, factor: float = 2):
+        super(Up, self).__init__()
+        self.up = nn.Upsample(scale_factor=factor, mode='nearest')
+        self.conv = nn.Sequential(
+            nn.Conv2d(c1+c2, c_out, 3, padding=1, padding_mode='zeros'),
+            nn.ReLU()
+        )
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor):
+        # x1 - batch_size, c1, h, w
+        # x2 - batch_size, c2, 2*h, 2*w
+        x1 = self.up(x1)  # batch_size, c1, 2*h, 2*w
+        x = torch.cat([x1, x2], dim=1)  # batch_size, c1 + c2, 2*h, 2*w
+        x = self.conv(x)  # batch_size, c_out, 2*h, 2*w
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        self.input = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, padding_mode='zeros'),
+            nn.ReLU()
+        )
+        self.down1 = nn.Sequential(
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 3, padding=1, padding_mode='zeros'),
+            nn.ReLU()
+        )
+        self.down2 = nn.Sequential(
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 3, padding=1, padding_mode='zeros'),
+            nn.ReLU()
+        )
+        self.down3 = nn.Sequential(
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 3, padding=1, padding_mode='zeros'),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, padding=1, padding_mode='zeros'),
+        )
+
+        self.up1 = Up(64, 32, 64)
+        self.up2 = Up(64, 32, 64)
+        self.up3 = Up(64, 32, 32)
+
+        self.out = nn.Sequential(
+            nn.Conv2d(32, 16, 3),
+            nn.Conv2d(16, 16, 3),
+            nn.Conv2d(16, 1, 3)
+        )
+
+    def forward(self, x: torch.Tensor):
+        # x - (batch_size, 64, 64)
+        x = x.unsqueeze(1)  # batch_size, 1, 64, 64
+        x1 = self.input(x)  # batch_size, 32, 64, 64
+        x2 = self.down1(x1)  # batch_size, 32, 32, 32
+        x3 = self.down2(x2)  # batch_size, 32, 16, 16
+        x4 = self.down3(x3)  # batch_size, 64, 8, 8
+
+        x = self.up1(x4, x3)  # batch_size, 64, 16, 16
+        x = self.up2(x, x2)  # batch_size, 64, 32, 32
+        x = self.up3(x, x1)  # batch_size, 32, 64, 64
+
+        x = self.out(x)  # batch_size, 1, 64, 64
+        x = x.squeeze(x, dim=1)  # batch_size, 64, 64
+        return x
