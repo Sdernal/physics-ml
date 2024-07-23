3fada9fdbd55f89b7461b6295e5e27b561fb3326
Sdernal <sdernal2@gmail.com>
2024-07-23 12:04:27 +0300

    Optimizing dataset generation

--- fluid_sim2.py
+++ fluid_sim2.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 class Fluid:
     def __init__(self):
         self.nt = 200
-        self.ngs = 20
+        self.ngs = 32
         self.nx = 64
         self.ny = 64
         self.h = 1.
@@ -35,6 +35,19 @@ class Fluid:
                     for j in range(1, self.ny - 1):
                         self.p[i, j] = (self.p[i + 1, j] + self.p[i - 1, j] + self.p[i, j + 1] + self.p[i, j - 1]
                                         - dx ** 2 * b[i, j]) / 4
+                self.p[:, 0] = 0
+                self.p[:, -1] = 0
+
+    def solve_v(self, dt):
+        b = np.zeros((self.nx, self.ny))
+        dx = dy = self.h
+        for _ in range(self.nt):
+            b[1:-1, 1:-1] = ((self.u[2:, 1:-1] - self.u[:-2, 1:-1]) / (2*dx)
+                             + (self.v[1:-1, 2:] - self.v[1:-1, :-2]) / (2*dy)) * self.rho / dt
+            for _ in range(self.ngs):
+                self.p[1:-1, 1:-1] = (self.p[2:, 1:-1] + self.p[:-2, 1:-1] + self.p[1:-1, 2:] + self.p[1:-1,:-2]
+                                        -b[1:-1, 1:-1] * dx**2) / 4
+
                 self.p[:, 0] = 0
                 self.p[:, -1] = 0
 
@@ -73,12 +86,49 @@ class Fluid:
         self.v = new_v
         self.u = new_u
 
+    def advect_v(self, dt):
+        new_u = self.u.copy()
+        new_v = self.v.copy()
+        dx = dy = self.h
+
+        new_u[1:-1, 1:-1] = self.u[1:-1, 1:-1] - (
+            self.u[1:-1, 1:-1] * (self.u[1:-1, 1:-1] - self.u[:-2, 1:-1]) / dx
+            + self.v[1:-1, 1:-1] * (self.u[1:-1, 1:-1] - self.u[1:-1, :-2]) / dy
+            + (self.p[2:, 1:-1] - self.p[:-2, 1:-1]) / (2*self.rho*dx)
+            + (
+                (self.u[2:, 1:-1] - self.u[1:-1, 1:-1] * 2 + self.u[:-2, 1:-1]) / dx**2
+                + (self.u[1:-1, 2:] - self.u[1:-1, 1:-1] * 2 + self.u[1:-1, :-2]) / dy**2
+            ) * self.nu
+        ) * dt
+
+        new_v[1:-1, 1:-1] = self.v[1:-1, 1:-1] - (
+            self.u[1:-1, 1:-1] * (self.u[1:-1,1:-1] - self.u[:-2, 1:-1]) / dx
+            + self.v[1:-1, 1:-1] * (self.v[1:-1,1:-1] - self.v[1:-1, :-2]) / dy
+            + (self.p[1:-1, 2:] - self.p[1:-1, :-2]) / (2* self.rho * dy)
+            + (
+                (self.v[2:, 1:-1] - self.v[1:-1, 1:-1] * 2 + self.v[:-2, 1:-1]) / dx**2
+                + (self.v[1:-1, 2:] - self.v[1:-1, 1:-1] * 2 + self.v[1:-1, :-2]) / dy**2
+            ) * self.nu
+        ) * dt
+
+        # extrapolation
+        new_u[0, :] = new_u[1, :]
+        new_u[-1, :] = new_u[-2, :]
+        new_v[0, :] = new_v[1, :]
+        new_v[-1, :] = new_v[-2, :]
+
+        self.v = new_v
+        self.u = new_u
+
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('--dst', type=str, default="data.npz", help="Path to store dataset. Should end with .npz")
+    arg_parser.add_argument('--dst', type=str, default="data.npz",
+                            help="Path to store dataset. Should end with .npz")
     arg_parser.add_argument('--frames', type=int, default=100, help="Length of data in frames")
     arg_parser.add_argument('--dt', type=float, default=0.001, help="Time between iterations")
+    arg_parser.add_argument('--fast', action="store_true", default=False,
+                            help="Apply vector operations to increase speed")
     args = arg_parser.parse_args()
 
     fluid = Fluid()
@@ -91,8 +141,12 @@ if __name__ == "__main__":
     ut.append(fluid.u.copy())
     for frame in tqdm(range(args.frames)):
         # make step and save data
-        fluid.solve(args.dt)
-        fluid.advect(args.dt)
+        if args.fast:
+            fluid.solve_v(args.dt)
+            fluid.advect_v(args.dt)
+        else:
+            fluid.solve(args.dt)
+            fluid.advect(args.dt)
 
         pt.append(fluid.p.copy())
         vt.append(fluid.v.copy())
